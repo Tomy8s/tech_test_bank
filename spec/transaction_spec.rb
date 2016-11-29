b874ed702add74490bf3165f2a12a2d62293236c
Tomy8s <tomy8s@hotmail.com>
2016-11-29 12:54:16 +0000

    test for Transaction#new_balance

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Transaction do
-  let(:subject_in) { Transaction.new(Date.new(2016,11,29), 50) }
-  let(:subject_out) { Transaction.new(Date.new(2016,11,29), -50) }
+  let(:subject_in) { Transaction.new(Date.new(2016,11,29), 50, 100) }
+  let(:subject_out) { Transaction.new(Date.new(2016,11,29), -50, 100) }
 
   describe '#date' do
     it 'records the date of a transaction' do
@@ -20,4 +20,13 @@ describe Transaction do
       expect(subject_out.balance_change).to eq -50
     end
   end
+
+  describe '#new_balance'
+    it 'calculates the new balance, given the initial balance and a debiting balance change' do
+      expect(subject_in.new_balance).to eq 150
+    end
+
+    it 'calculates the new balance, given the initial balance and a withdrawl balance change' do
+      expect(subject_out.new_balance).to eq 50
+    end
 end
