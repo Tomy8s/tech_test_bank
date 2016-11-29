b74f35fc61d64034f2590a60c8f8d55f04d51cfa
Tomy8s <tomy8s@hotmail.com>
2016-11-29 13:53:42 +0000

    Account#transaction_history init as empty array

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,9 @@
 class Account
-  attr_reader :customer_full_name, :current_balance
+  attr_reader :customer_full_name, :current_balance, :transaction_history
 
   def initialize(customer_full_name, opening_balance = 100)
     @customer_full_name = customer_full_name
     @current_balance = opening_balance
+    @transaction_history = []
   end
 end
