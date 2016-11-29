968d1f9261ee2559914b123197d0818387234de7
Tomy8s <tomy8s@hotmail.com>
2016-11-29 17:15:39 +0000

    test for Bank#print_headings returning headings

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -6,4 +6,10 @@ describe Bank do
       expect(subject.class).to respond_to('print_statement').with(1).argument
     end
   end
+
+  describe '#print_headings' do
+    it 'prints the correct headings' do
+      expect(subject.print_headings).to eq 'date       || credit || debit || balance'
+    end
+  end
 end
