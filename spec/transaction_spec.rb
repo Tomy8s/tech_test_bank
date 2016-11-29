a4722f1a97938b6a3059f78d3d5cc453a7226814
Tomy8s <tomy8s@hotmail.com>
2016-11-29 12:46:44 +0000

    test for Transaction#change_balance

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,12 +1,24 @@
 require 'spec_helper'
 
 describe Transaction do
-  let(:subject) { Transaction.new(Date.new(2016,11,29)) }
+  # let(:subject) { Transaction.new(Date.new(2016,11,29)) }
 
   describe '#date' do
     it 'records the date of a transaction' do
+      subject = Transaction.new(Date.new(2016,11,29))
       expect(subject.date.to_s).to eq "2016-11-29"
     end
   end
 
+  describe '#balance_change' do
+    it 'records the positive change in balance' do
+      subject = Transaction.new(Date.new(2016,11,29), 50)
+      expect(subject.balance_change).to eq 50
+    end
+
+    it 'records the negative change in balance' do
+      subject = Transaction.new(Date.new(2016,11,29), -50)
+      expect(subject.balance_change).to eq -50
+    end
+  end
 end
