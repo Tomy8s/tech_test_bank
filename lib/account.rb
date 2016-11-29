2952eb3f3964d579c3c0a604de54c0ea94883814
Tomy8s <tomy8s@hotmail.com>
2016-11-29 13:30:10 +0000

    Account#current_balance initialized as 100 by default

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,8 @@
 class Account
-  attr_reader :customer_full_name
+  attr_reader :customer_full_name, :current_balance
 
-  def initialize(customer_full_name)
+  def initialize(customer_full_name, opening_balance = 100)
     @customer_full_name = customer_full_name
+    @current_balance = opening_balance
   end
 end
