018252e33b28f5f822aea4f5323a24a0022ab7cf
Tomy8s <tomy8s@hotmail.com>
2016-11-29 18:46:27 +0000

    Bank::print_transactions prints debit or credit

--- lib/bank.rb
+++ lib/bank.rb
@@ -10,7 +10,7 @@ class Bank
   def self.print_transactions(account)
     transactions = account.transaction_history.reverse
     transactions.each do |transact|
-      puts "#{transact.date.strftime('%d/%m/%Y')} || #{'%.2f' % transact.new_balance}"
+      puts "#{transact.date.strftime('%d/%m/%Y')} || #{transact.balance_change > 0 ? '%.2f' % transact.balance_change : '       '} || #{transact.balance_change < 0 ? '%.2f' % transact.balance_change.abs : '       '} || #{'%.2f' % transact.new_balance}"
     end
   end
 end
