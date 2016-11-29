ec4be706a5e6564861f5328e6858593fe967c673
Tomy8s <tomy8s@hotmail.com>
2016-11-29 12:50:06 +0000

    refactor to use let and fix first breaking test

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
 
 describe Transaction do
-  # let(:subject) { Transaction.new(Date.new(2016,11,29)) }
+  let(:subject_in) { Transaction.new(Date.new(2016,11,29), 50) }
+  let(:subject_out) { Transaction.new(Date.new(2016,11,29), -50) }
 
   describe '#date' do
     it 'records the date of a transaction' do
-      subject = Transaction.new(Date.new(2016,11,29))
-      expect(subject.date.to_s).to eq "2016-11-29"
+      expect(subject_in.date.to_s).to eq "2016-11-29"
     end
   end
 
   describe '#balance_change' do
     it 'records the positive change in balance' do
-      subject = Transaction.new(Date.new(2016,11,29), 50)
-      expect(subject.balance_change).to eq 50
+      expect(subject_in.balance_change).to eq 50
     end
 
     it 'records the negative change in balance' do
       subject = Transaction.new(Date.new(2016,11,29), -50)
-      expect(subject.balance_change).to eq -50
+      expect(subject_out.balance_change).to eq -50
     end
   end
 end
